d522c1f287228cf8fe61fd0be1e6fc82e283b1d1
Bakred <bakred90@gmail.com>
2016-11-27 02:49:53 +0100

    Premier commit intelliJ

---
+++ src/arg/controlleur/Test.java
@@ -0,0 +1,7 @@
+package arg.controlleur;
+
+/**
+ * Created by simon on 27/11/16.
+ */
+public class Test {
+}
--- src/arg/main/Main.java
+++ src/arg/main/Main.java
@@ -7,7 +7,7 @@ import arg.vue.swing.Fenetre;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		System.setProperty("sun.java2d.opengl", "true");
-		Fenetre fen = new Fenetre();
+		//Fenetre fen = new Fenetre();
 		Music.ChoixMusic();
 		// Music.PlayMusic("./music/distant_lullaby.ogg");
 		MenuPrincipal.Title();
